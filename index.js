e69a75452bcdb9add7574df7083039a1f11b24ed
Hannah Malm <hannahemalm616@gmail.com>
2021-12-29 11:23:04 -0500

    commit tree 34a0f31ddd48dcb52c69e973cd77ce70e99bcf69

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+// String which specifies the 24-hour time in the format HH:MM
+//If the time is earlier than 12pm, return "Good Morning".
+//If the time is between 12pm and 5pm, return "Good Afternoon".
+//If the time is later than 5pm, return "Good Evening".
+function greet(time){
+  if (time < 12) {
+    greeting = "Good morning";
+  } else if (time < 12 && time < 17) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+//
+function displayMessage(){
+
+}
